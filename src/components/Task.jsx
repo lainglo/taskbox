782f3178d4ad1d177e3733a35124f51a6c49c626
MC Rainman <llourens@csir.co.za>
2024-01-16 15:16:15 +0200

    adds overflow story

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -29,7 +29,7 @@ export default function Task({
           readOnly={true}
           name="title"
           placeholder="Input title"
-          style={{ backgroundColor: "red" }}
+          style={{ textOverflow: "ellipsis" }}
         />
       </label>
 
